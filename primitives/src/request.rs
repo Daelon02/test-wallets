52e33c11b42ded36c823309a2eb6796c706e102d
Daelon02 <Daelon02@github.com>
2021-08-20 10:27:16 +0300

    first commit

---
+++ primitives/src/request.rs
@@ -0,0 +1,91 @@
+use realis_primitives::{Basic, Rarity, TokenId};
+use serde::{Deserialize, Deserializer, Serialize};
+
+pub type Version = String;
+pub type Topic = String;
+pub type TopicRes = String;
+pub type Lang = String;
+pub type Id = String;
+
+pub type Amount = u128;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Raw<T> {
+    pub version: Version,
+    pub topic: Topic,
+    pub topic_res: TopicRes,
+    pub lang: Lang,
+    pub id: Id,
+
+    pub params: T,
+}
+
+// CreditHardCurrency, DebitHardCurrency
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct TransferToBSC {
+    pub account_id: String,
+    pub bsc_account: String,
+    #[serde(deserialize_with = "u128_from_any")]
+    pub amount: Amount,
+}
+
+// CreditHardCurrency, DebitHardCurrency
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct TransferToRealis {
+    pub account_id: String,
+    pub bsc_account: String,
+    #[serde(deserialize_with = "u128_from_any")]
+    pub amount: Amount,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct AddNftToBsc {
+    pub account_id: String,
+    pub bsc_account: String,
+    pub token_id: TokenId,
+    pub token_type: Basic,
+    pub rarity: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct AddNftToRealis {
+    pub bsc_account: String,
+    pub account_id: String,
+    pub token_id: TokenId,
+    pub token_type: Basic,
+    pub rarity: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct WithdrawToBsc {
+    pub account_id: String,
+    pub bsc_account: String,
+    #[serde(deserialize_with = "u128_from_any")]
+    pub amount: Amount,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct WithdrawToRealis {
+    pub bsc_account: String,
+    pub account_id: String,
+    #[serde(deserialize_with = "u128_from_any")]
+    pub amount: Amount,
+}
+
+/// # Errors
+pub fn u128_from_any<'de, D>(deserializer: D) -> Result<u128, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    #[derive(Deserialize)]
+    #[serde(untagged)]
+    enum StrOrU64<'a> {
+        Str(&'a str),
+        U64(u64),
+    }
+
+    Ok(match StrOrU64::deserialize(deserializer)? {
+        StrOrU64::Str(v) => v.parse().unwrap_or(0), // Ignoring parsing errors
+        StrOrU64::U64(v) => v.into(),
+    })
+}
---
+++ utils/src/lib.rs
@@ -0,0 +1,81 @@
+pub mod parse;
+
+pub mod contract {
+    use std::{str::FromStr, time::Duration};
+
+    use log::{error, info};
+    use tokio::time::sleep;
+    use web3::{contract::Contract, transports::WebSocket, types::Address, Web3};
+
+    pub async fn connect() -> Web3<WebSocket> {
+        let url = "wss://data-seed-prebsc-1-s1.binance.org:8545";
+        // Connect to bsc
+        let mut wss = WebSocket::new(url).await;
+        loop {
+            match wss {
+                Ok(_) => break,
+                Err(error) => {
+                    error!("Cannot connect {:?}", error);
+                    info!("Try reconnect");
+                    wss = WebSocket::new(url).await;
+                }
+            }
+            // Wait a bit before reconnect
+            sleep(Duration::from_millis(1000)).await;
+        }
+
+        web3::Web3::new(wss.unwrap())
+    }
+
+    /// # Panics
+    ///
+    /// Create new token contract
+    pub async fn token_new() -> Contract<WebSocket> {
+        let web3 = connect().await;
+        // TODO get from config file
+        let address: Address =
+            Address::from_str("0x6d749dD747da1754Ef16B3fA2E779834CF636805")
+                .unwrap();
+
+        let json_abi = include_bytes!("./../res/BEP20.abi");
+
+        Contract::from_json(web3.eth(), address, json_abi).unwrap()
+    }
+
+    /// # Panics
+    ///
+    /// Create new nft contract
+    pub async fn nft_new() -> Contract<WebSocket> {
+        let web3 = connect().await;
+        // TODO get from config file
+        let address: Address =
+            Address::from_str("0x47837cc63ea6A912e699f9d2D4AeEb5C17F385aB")
+                .unwrap();
+
+        let json_abi = include_bytes!("./../res/BEP721.abi");
+
+        Contract::from_json(web3.eth(), address, json_abi).unwrap()
+    }
+}
+
+pub mod accounts {
+    // use sp_core::Pair;
+    use std::{fs, path::Path, str::FromStr};
+
+    use secp256k1::SecretKey;
+
+    /// # Panics
+    ///
+    /// Read private key from file
+    pub fn bsc<P: AsRef<Path>>(path: P) -> SecretKey {
+        let string = fs::read_to_string(&path).unwrap();
+        SecretKey::from_str(&string).unwrap()
+    }
+
+    // pub fn realis<P: AsRef<Path>>(path: P, password: Option<&str>) -> Pair<>
+    // {     Pair::from_string(
+    //         from_path_to_account(path),
+    //         password,
+    //     ).unwrap()
+    // }
+}
